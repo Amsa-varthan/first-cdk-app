a9e4f94ac08c7268b0adedd9163b1ef614f72605
Amsa-varthan <amsavarthan448@gmail.com>
2025-07-26 15:37:08 +0530

    Initial commit of my first CDK app

--- lib/first cdk-stack.ts
+++ lib/first cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'FirstCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new Function(this, 'Hello-World-Cdk', {
+      runtime: Runtime.NODEJS_20_X, // Corrected line
+      memorySize: 512,
+      handler: 'demo.handler',
+      code: Code.fromAsset(join(__dirname, '../lambdas'))
+    });
   }
 }
